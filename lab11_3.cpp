1c953f30171c222d7192fe1d3c8c013ae2aefae9
___.flxk.__ <89276072+fluk3G123@users.noreply.github.com>
2023-01-20 17:43:16 +0700

    love love

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include<fstream>
+using namespace std;
+
+int main(){
+    ifstream source ;
+    string textline ;
+    double sum=0 , count=0 , Mean , sum2=0 , SD;
+    source.open("score.txt");
+
+    while (getline(source,textline))
+    {
+        sum = sum + stod(textline);
+        sum2 = sum2 + pow(stod(textline),2);
+        count++;
+
+    }
+    Mean = sum/count;
+    SD = sqrt(sum2/count-pow(Mean,2));
+
+    cout << "Number of data = "<<count << endl;
+    cout << setprecision(3);
+    cout << "Mean = "<<Mean << endl;
+    cout << "Standard deviation = "<<SD;
+}
